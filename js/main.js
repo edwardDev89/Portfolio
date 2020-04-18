15b74b7e70401ae283e319a13d34a9bef23897b0
edwardDev89 <63137348+edwardDev89@users.noreply.github.com>
2020-04-18 21:05:10 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+//To initiate owl carousel slide
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 7,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000
+    })
+  });
